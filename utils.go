9a4bd5bed33f07bc66d1387edc2f072d5bc204cc
Saeed Rasooli <saeed.gnu@gmail.com>
2017-09-24 09:58:16 +0800

    remove ununsed function 'convert'

--- utils.go
+++ utils.go
@@ -23,28 +23,3 @@ func mapFromKeyValuePairs(kvPairs ...interface{}) map[string]interface{} {
 	}
 	return m
 }
-
-func convert(key string, valueIn interface{}, typ reflect.Type) (valueOut interface{}, err error) {
-	if reflect.TypeOf(valueIn) == typ {
-		valueOut = valueIn
-		return
-	}
-	defer func() {
-		r := recover()
-		if r != nil {
-			err = NewError(
-				InvalidArgument,
-				fmt.Sprintf(
-					"invalid '%v', must be %v",
-					key,
-					typ,
-				),
-				fmt.Errorf("panic in convert: %v", r),
-			)
-		}
-	}()
-	v := reflect.ValueOf(valueIn)
-	v2 := v.Convert(typ)
-	valueOut = v2.Interface()
-	return
-}
