40354d4a5d306684abb94912093d2c9a88660093
Saeed Rasooli <saeed.gnu@gmail.com>
2017-11-12 17:44:06 +0800

    test default errorDispatcher before disabling it on test

--- init_test.go
+++ init_test.go
@@ -1,5 +1,17 @@
 package restpc
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func init() {
-	errorDispatcher = func(request Request, rpcErr RPCError) {}
+	r, _ := http.NewRequest("GET", "", nil)
+	request := &requestImp{
+		r:           r,
+		handlerName: "foo",
+	}
+	rpcErr := NewError(Unavailable, "", fmt.Errorf("boo")).Add("foo", "bar")
+	errorDispatcher(request, rpcErr)
+	SetErrorDispatcher(func(request Request, rpcErr RPCError) {})
 }
