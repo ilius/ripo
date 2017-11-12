7b11a238f5c1a35f3a42aeff1f6d3ba92fcab784
Saeed Rasooli <saeed.gnu@gmail.com>
2017-11-12 23:30:36 +0800

    add TestFromBody_GetStringList

--- from_body_test.go
+++ from_body_test.go
@@ -51,3 +51,62 @@ func TestFromBody_GetString(t *testing.T) {
 		assert.Nil(t, err)
 	}
 }
+func TestFromBody_GetStringList(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockReq := NewMockRequest(ctrl)
+	var req Request = mockReq
+	{
+		mockReq.EXPECT().BodyMap().Return(nil, fmt.Errorf("unknown error"))
+		value, err := FromBody.GetStringList(req, "names")
+		assert.Nil(t, value)
+		assert.EqualError(t, err, "unknown error")
+	}
+	{
+		mockReq.EXPECT().BodyMap().Return(nil, nil)
+		value, err := FromBody.GetStringList(req, "names")
+		assert.Nil(t, value)
+		assert.Nil(t, err)
+	}
+	{
+		mockReq.EXPECT().BodyMap().Return(map[string]interface{}{
+			"names": 123,
+		}, nil)
+		value, err := FromBody.GetStringList(req, "names")
+		assert.Nil(t, value)
+		assert.EqualError(t, err, "invalid 'names', must be array of strings")
+	}
+	{
+		mockReq.EXPECT().BodyMap().Return(map[string]interface{}{
+			"names": "John Smith",
+		}, nil)
+		value, err := FromBody.GetStringList(req, "names")
+		assert.Nil(t, value)
+		assert.EqualError(t, err, "invalid 'names', must be array of strings")
+	}
+	{
+		mockReq.EXPECT().BodyMap().Return(map[string]interface{}{
+			"names": []interface{}{"John Smith", 1234},
+		}, nil)
+		value, err := FromBody.GetStringList(req, "names")
+		assert.Nil(t, value)
+		assert.EqualError(t, err, "invalid 'names', must be array of strings")
+	}
+	{
+		mockReq.EXPECT().BodyMap().Return(map[string]interface{}{
+			"names": []string{"John Smith", "John Doe"},
+		}, nil)
+		value, err := FromBody.GetStringList(req, "names")
+		assert.Equal(t, []string{"John Smith", "John Doe"}, value)
+		assert.Nil(t, err)
+	}
+	{
+		mockReq.EXPECT().BodyMap().Return(map[string]interface{}{
+			"names": []interface{}{"John Smith", "John Doe"},
+		}, nil)
+		value, err := FromBody.GetStringList(req, "names")
+		assert.Equal(t, []string{"John Smith", "John Doe"}, value)
+		assert.Nil(t, err)
+	}
+
+}
