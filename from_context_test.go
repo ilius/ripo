1d629ce39f625ae346dc94f23209a4515bd1898a
Saeed Rasooli <saeed.gnu@gmail.com>
2017-11-13 23:01:09 +0800

    add TestFromContext_GetStringList

--- from_context_test.go
+++ from_context_test.go
@@ -38,3 +38,34 @@ func TestFromContext_GetString(t *testing.T) {
 		assert.NoError(t, err)
 	}
 }
+
+func TestFromContext_GetStringList(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockReq := NewMockRequest(ctrl)
+	var req Request = mockReq
+	{
+		mockReq.EXPECT().Context().Return(context.Background())
+		value, err := FromContext.GetStringList(req, "names")
+		assert.Nil(t, value)
+		assert.NoError(t, err)
+	}
+	{
+		mockReq.EXPECT().Context().Return(context.WithValue(context.Background(), "names", 123))
+		value, err := FromContext.GetStringList(req, "names")
+		assert.Nil(t, value)
+		assert.EqualError(t, err, "invalid 'names', must be array of strings")
+	}
+	{
+		mockReq.EXPECT().Context().Return(context.WithValue(context.Background(), "names", "John"))
+		value, err := FromContext.GetStringList(req, "names")
+		assert.Nil(t, value)
+		assert.EqualError(t, err, "invalid 'names', must be array of strings")
+	}
+	{
+		mockReq.EXPECT().Context().Return(context.WithValue(context.Background(), "names", []string{"John", "Smith"}))
+		value, err := FromContext.GetStringList(req, "names")
+		assert.Equal(t, []string{"John", "Smith"}, value)
+		assert.NoError(t, err)
+	}
+}
