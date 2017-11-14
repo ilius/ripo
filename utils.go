558ca0dcaf761ef57601425638c03a299aa0a00c
Saeed Rasooli <saeed.gnu@gmail.com>
2017-11-15 00:23:38 +0800

    remove unused func mapFromKeyValuePairs

--- utils.go
+++ utils.go
@@ -1,25 +1,10 @@
 package restpc
 
 import (
-	"fmt"
 	"reflect"
 	"runtime"
 )
 
 func getFunctionName(i interface{}) string {
 	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
-}
-
-func mapFromKeyValuePairs(kvPairs ...interface{}) map[string]interface{} {
-	if len(kvPairs)%2 != 0 {
-		panic(fmt.Sprintf(
-			"mapFromKeyValuePairs: must give even number of args, give %v",
-			len(kvPairs),
-		))
-	}
-	m := map[string]interface{}{}
-	for i := 0; i < len(kvPairs)/2; i++ {
-		m[fmt.Sprintf("%v", kvPairs[i])] = kvPairs[i+1]
-	}
-	return m
 }
