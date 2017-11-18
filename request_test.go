3b867b2c73421831c8b9d4739ca9f9a598d1e9f6
Saeed Rasooli <saeed.gnu@gmail.com>
2017-11-18 23:05:12 +0800

    add TestSetDefaultParamSources

--- request_test.go
+++ request_test.go
@@ -1,12 +1,41 @@
 package restpc
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func callSetDefaultParamSources(sources ...FromX) (err error) {
+	defer func() {
+		r := recover()
+		if r != nil {
+			err = fmt.Errorf("Panic: %v", r)
+		}
+	}()
+	SetDefaultParamSources(sources...)
+	return
+}
+
+func TestSetDefaultParamSources(t *testing.T) {
+	defer func() {
+		r := recover()
+		if r != nil {
+			t.Errorf("Panic: %v", r)
+		}
+	}()
+	{
+		err := callSetDefaultParamSources()
+		assert.EqualError(t, err, "Panic: SetDefaultParamSources: no arguments given")
+	}
+	assert.NoError(t, callSetDefaultParamSources(FromBody))
+	assert.NoError(t, callSetDefaultParamSources(FromBody, FromContext))
+	assert.NoError(t, callSetDefaultParamSources(FromBody, FromContext, FromEmpty, FromEmpty))
+	assert.NoError(t, callSetDefaultParamSources(FromEmpty))
+}
 
 func Test_requestImp_URL(t *testing.T) {
 	r, err := http.NewRequest("GET", "http://127.0.0.1/test1", nil)
