0c48bf46b667a4764a0920a9cdf8f75f4e74d9d0
Saeed Rasooli <saeed.gnu@gmail.com>
2017-11-23 23:26:58 +0800

    move FromX interface from request.go to from_x.go, just to remove unneeded MockFromX from mock_request.go

--- request.go
+++ request.go
@@ -36,15 +36,6 @@ type Request interface {
 	FullMap() map[string]interface{}
 }
 
-type FromX interface {
-	GetString(req Request, key string) (*string, error)
-	GetStringList(req Request, key string) ([]string, error)
-	GetInt(req Request, key string) (*int, error)
-	GetFloat(req Request, key string) (*float64, error)
-	GetBool(req Request, key string) (*bool, error)
-	GetTime(req Request, key string) (*time.Time, error)
-}
-
 var defaultParamSources = []FromX{
 	FromBody,
 	FromForm,
