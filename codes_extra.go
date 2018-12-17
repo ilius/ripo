dfce912a31811762ad660e76460b46c944421d68
Saeed Rasooli <saeed.gnu@gmail.com>
2018-12-17 22:27:56 +0800

    add status code ResourceLocked = 18

--- codes_extra.go
+++ codes_extra.go
@@ -5,4 +5,9 @@ const (
 	// MissingArgument means that an input parameter is required but not given
 	// either missing in the input/request, or has empty value
 	MissingArgument Code = 17
+
+	// ResourceLocked: added by Saeed Rasooli
+	// ResourceLocked means that the give resource is currently busy or temporarily locked
+	// by abother request (either by the same user or another user)
+	ResourceLocked Code = 18
 )
